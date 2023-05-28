e8e9e98434bf7a576a3d78fa53d0a961ac8cc9f0
Sirio1593 <128339037+Sirio1593@users.noreply.github.com>
2023-05-28 18:22:54 -0300

    Agregado de clase

--- Proyecto02/views.py
+++ Proyecto02/views.py
@@ -1,4 +1,9 @@
+import datetime as dt
 from django.http import HttpResponse
 
 def saludo(request):
     return HttpResponse("<br><br><br>¡Hola mundo!<br><br>  ")
+
+def dia(request):
+    dia = dt.datetime.now()
+    return HttpResponse(f"<br><br><br>La fecha y hora de hoy es {dia}<br><br>  ")
