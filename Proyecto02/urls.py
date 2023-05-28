8dfd437d4f48379bb72524d71f260bccc2eba9c1
Sirio1593 <128339037+Sirio1593@users.noreply.github.com>
2023-05-28 18:23:58 -0300

    Agregado en Url --> Clase Dia

--- Proyecto02/urls.py
+++ Proyecto02/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from .views import saludo
+from .views import dia
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('ss/', saludo),
+    path('d/', dia),
 ]
